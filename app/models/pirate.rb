4a1bd3c74bf70c1924d0fe4aa63784f581a15b25
Justin Tooley <jstooley823@gmail.com>
2017-11-13 15:18:05 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,12 +1,12 @@
 class Pirate
   attr_writer :name, :weight, :height
-
+  PIRATES = []
   def initialize(params)
     @name = params['pirate']['name']
     @weight = params['pirate']['weight']
     @height = params['pirate']['height']
   end
 
-  def self.all 
+  def self.all
   end
 end
